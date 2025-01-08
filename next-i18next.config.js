c9561b98d3d9380d94541199e8d3c022e4f3e71c
NotBlue-Dev <enzodubocage@gmail.com>
2025-01-09 00:12:24 +0100

    update i18n config

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,9 +1,18 @@
 /** @type {import('next-i18next').UserConfig} */
 
+import path from 'path';
+
 module.exports = {
     i18n: {
         locales: ['en-US', 'fr'],
         defaultLocale: 'fr',
         localeDetection:false,
     },
+    fallbackLng: {
+        default: ['en-US'],
+        'fr': ['en-US'],
+        'en-US': ['fr'],
+        nonExplicitSupportedLngs: true,
+    },
+    localePath: path.resolve('./public/locales')
   }
